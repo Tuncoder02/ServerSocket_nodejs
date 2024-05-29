87753e23fab6c718d0fb53f12ace079f9959fb1b
Trần Anh Tuấn <95674899+Tuncoder02@users.noreply.github.com>
2024-05-29 22:38:13 +0700

    ma hoa db

--- encryption.js
+++ encryption.js
@@ -2,6 +2,8 @@ const crypto = require('crypto');
 
 // Convert key to 32 bytes (256 bits)
 const key = crypto.createHash('sha256').update('tuanhuysy').digest();
+const key2 = crypto.createHash('sha256').update('khacsinh').digest();
+
 
 // Function to encrypt data
 function encrypt(text) {
@@ -21,4 +23,20 @@ function decrypt(encryptedText) {
     return decrypted;
 }
 
-module.exports = { encrypt, decrypt };
+function encryptWithKey2(text) {
+    const cipher = crypto.createCipheriv('aes-256-ecb', key2, null);
+    cipher.setAutoPadding(true); // PKCS7 padding is enabled by default
+    let encrypted = cipher.update(text, 'utf8', 'base64'); // Using base64 for compatibility
+    encrypted += cipher.final('base64');
+    return encrypted;
+}
+
+// Function to decrypt data with key2
+function decryptWithKey2(encryptedText) {
+    const decipher = crypto.createDecipheriv('aes-256-ecb', key2, null);
+    decipher.setAutoPadding(true); // PKCS7 padding is enabled by default
+    let decrypted = decipher.update(encryptedText, 'base64', 'utf8'); // Using base64 for compatibility
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+module.exports = { encrypt, decrypt,encryptWithKey2,decryptWithKey2 };
